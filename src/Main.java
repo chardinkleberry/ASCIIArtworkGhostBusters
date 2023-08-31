0ce6e3eb89d5f1aa79bbd9d4d558ab3c6c6c2810
Charlie Liu <100143091@lubbockisd.net>
2023-08-31 09:33:53 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("                       ---                                     \n" +
+                "                    -        --                             \n" +
+                "                --( /     \\ )XXXXXXXXXXXXX                   \n" +
+                "            --XXX(   O   O  )XXXXXXXXXXXXXXX-              \n" +
+                "           /XXX(       U     )        XXXXXXX\\               \n" +
+                "         /XXXXX(              )--   XXXXXXXXXXX\\             ");
+        System.out.println("        /XXXXX/ (      O     )   XXXXXX   \\XXXXX\\\n" +
+                "        XXXXX/   /            XXXXXX   \\   \\XXXXX----        \n" +
+                "        XXXXXX  /          XXXXXX         \\  ----  -         \n" +
+                "---     XXX  /          XXXXXX      \\           ---        ");
+        System.out.println("  --  --  /      /\\  XXXXXX            /     ---=         \n" +
+                "    -        /    XXXXXX              '--- XXXXXX         \n" +
+                "      --\\/XXX\\ XXXXXX                      /XXXXX         ");
+        System.out.println("        \\XXXXXXXXX                        /XXXXX/\n" +
+                "         \\XXXXXX                         /XXXXX/         \n" +
+                "           \\XXXXX--  /                -- XXXX/       ");
+        System.out.println("            --XXXXXXX---------------  XXXXX--         \n" +
+                "               \\XXXXXXXXXXXXXXXXXXXXXXXX-            \n" +
+                "                 --XXXXXXXXXXXXXXXXXX-");
+
+        //https://www.asciiart.eu/movies/ghostbusters
+
+    }
+}
