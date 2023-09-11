7c3de0d4e3fcbb56cf7f7d80eb2066829b619da2
Charlie Liu <100143091@lubbockisd.net>
2023-09-11 09:40:45 -0500

    ipush

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("                       ---                                     \n" +
-                "                    -        --                             \n" +
-                "                --( /     \\ )XXXXXXXXXXXXX                   \n" +
-                "            --XXX(   O   O  )XXXXXXXXXXXXXXX-              \n" +
-                "           /XXX(       U     )        XXXXXXX\\               \n" +
-                "         /XXXXX(              )--   XXXXXXXXXXX\\             ");
-        System.out.println("        /XXXXX/ (      O     )   XXXXXX   \\XXXXX\\\n" +
-                "        XXXXX/   /            XXXXXX   \\   \\XXXXX----        \n" +
-                "        XXXXXX  /          XXXXXX         \\  ----  -         \n" +
-                "---     XXX  /          XXXXXX      \\           ---        ");
-        System.out.println("  --  --  /      /\\  XXXXXX            /     ---=         \n" +
-                "    -        /    XXXXXX              '--- XXXXXX         \n" +
-                "      --\\/XXX\\ XXXXXX                      /XXXXX         ");
-        System.out.println("        \\XXXXXXXXX                        /XXXXX/\n" +
-                "         \\XXXXXX                         /XXXXX/         \n" +
-                "           \\XXXXX--  /                -- XXXX/       ");
-        System.out.println("            --XXXXXXX---------------  XXXXX--         \n" +
-                "               \\XXXXXXXXXXXXXXXXXXXXXXXX-            \n" +
-                "                 --XXXXXXXXXXXXXXXXXX-");
+public class Main
+{
+    public static void main ( String[] args )
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+        byte byteOne = 127;
+        short shortOne = -32123;
+        int intOne = 2100000000;
+        long longOne = -2147483645;
+        float floatOne = 12.12345F;
+        double doubleOne = 827.182;
+        char charOne = 'C';
+        boolean booleanOne = true;
+        String stringOne = ("hello world");
 
-        //https://www.asciiart.eu/movies/ghostbusters
+       System.out.println("/////////////////////////////////");
+        System.out.println("*Charlie Liu          10/31/2007*");
+        System.out.println("*                               *");
+        System.out.println("*         integer types         *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne =   "+byteOne+"\t\t*");
+        System.out.println("*16 bit - shortOne =  "+shortOne+"\t*");
+        System.out.println("*32 bit - intOne ="  +intOne+"\t*");
+        System.out.println("*64 bit - longOne =" +longOne+"\t*");
+        System.out.println("*          real types           *");
+        System.out.println("*                               *");
+        System.out.println("*32 bit - floatOne ="+floatOne+"\t*");
+        System.out.println("*64 bit - shortOne ="+doubleOne+"\t\t*");
+        System.out.println("*      other integer types      *");
+        System.out.println("*                               *");
+        System.out.println("*16 bit - charOne =     "+charOne+"\t\t*");
+        System.out.println("*           other types         *");
+        System.out.println("*                               *");
+        System.out.println("*booleanOne =     "+booleanOne+"\t\t    *");
+        System.out.println("*stringOne =     "+stringOne+"\t*");
+        System.out.println("/////////////////////////////////");
 
     }
 }
